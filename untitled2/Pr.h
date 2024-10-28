9a04cb13d7b02c47b9ac553557fe37f11c3caede
josealfred62 <159482144+josealfred62@users.noreply.github.com>
2024-10-27 21:19:57 -0600

    Add files via upload
    1

---
+++ untitled2/Pr.h
@@ -0,0 +1,172 @@
+#ifndef PR_H
+#define PR_H
+
+
+int cont[10]={0};
+char tarj[10][2];
+int punt[10]={0};
+int tf[10]={0};
+char usr[10][110];
+
+void Ana() {
+
+     usr[0][20]="AnaG123";
+
+    cont[0]=123456;
+
+
+    tarj[0][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom[100]={"Av. Siempreviva 123, Springfield"};
+    char corr[20]={"ana.garcia@email.com"};
+    tf[0]=5551234567;
+
+}
+
+void Juan() {
+
+    usr[1][100]="AnaG123";
+
+    cont[1]=123456;
+
+
+    tarj[1][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Mari() {
+
+    usr[3][100]="AnaG123";
+
+    cont[2]=123456;
+
+
+    tarj[2][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Pedro() {
+
+    usr[4][100]="AnaG123";
+    cont[1]=123456;
+
+
+    tarj[3][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Laura() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[4][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Carlos() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[5][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Sofia() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[6][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void David() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[7][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Isabel() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[8][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+void Miguel() {
+
+
+
+    cont[1]=123456;
+
+
+    tarj[9][2]=2;
+
+
+    punt[0]=5000;
+
+    char dom2[100]={"Av. Siempreviva 123, Springfield"};
+    char corr2[20]={"ana.garcia@email.com"};
+    tf[1]=5559876543;
+}
+#endif // PR_H
---
+++ untitled2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Pr.h"
+
+int main() {
+    Ana();
+    Juan();
+    Mari();
+    Pedro();
+
+
+    printf("El Usuario es: %s\n", usr[0]);
+    printf("El numero es: %d\n", cont[0]);
+    printf("El numero es: %d\n", cont[1]);
+    printf("El numero es: %d\n", cont[2]);
+return 0;
+
+}
